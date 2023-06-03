1a0a161716c6f457aa4f90475b9666aaf963797f
Dan Putler <dan.putler@gmail.com>
2023-06-02 20:09:44 -0700

    Change the header to do just a single iter

--- src/wbgt.h
+++ src/wbgt.h
@@ -78,8 +78,8 @@
 #define	NORMSOLAR_MAX 0.85
 #define	REF_HEIGHT	2.0 
 #define	MIN_SPEED	0.13
-#define	CONVERGENCE	0.02
-#define	MAX_ITER	500
+#define	CONVERGENCE	100.0
+#define	MAX_ITER	1
 
 /*
  * All function declarations in this file are extracted from Liljegren's original code 
